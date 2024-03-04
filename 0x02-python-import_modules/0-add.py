16c6d0631d8dd0c876b83185e80ba34b248dc302
zel-tes <zeltes18@gmail.com>
2024-03-04 01:00:46 -0800

    modules_1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from add_0 import add as ad
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, ad(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("10 + 5 =  ", add(a, b))
+print("10 - 5 =  ", sub(a, b))
+print("10 * 5 =  ", mul(a, b))
+print("10 / 5 =  ", div(a, b))
