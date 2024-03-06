b1ca5a5d773b9efb9b52845c4592091d49c473b6
zel-tes <zeltes18@gmail.com>
2024-03-06 04:48:40 -0800

    0x03_8 commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
+    elif my_list == []:
+        return None
     else:
         max_num = my_list[0]
         for i in range(len(my_list)):
