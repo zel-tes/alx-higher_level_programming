3f071b18261bee694bf9fcee7bcac230767ab20e
zel-tes <zeltes18@gmail.com>
2024-03-06 03:19:35 -0800

    0x03_5 commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end="")
+            if i < (len(row) - 1):
+                print(" ", end="")
         print()
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0 and len(tuple_b) == 0:
-         new_tuple = (0 + 0, 0 + 0)
+        new_tuple = (0 + 0, 0 + 0)
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         new_tuple = (tuple_a[0] + tuple_b[0], 0 + 0)
     elif len(tuple_a) == 0:
         new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
     elif len(tuple_b) == 0:
-       new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+        new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
     elif len(tuple_a) == 1 and len(tuple_b) > 1:
         new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
     elif len(tuple_a) > 1 and len(tuple_b) == 1:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
     else:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return new_tuple
+        return new_tuple
