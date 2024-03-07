9bdb587aa65f34cc1e6fcc5e2dae2a88875cac97
zel-tes <zeltes18@gmail.com>
2024-03-07 00:01:38 -0800

    0x02_5 commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    sum = 0
+elif len(sys.argv) > 1:
+    sum = 0
+    for n in range(1, len(sys.argv)):
+        sum += int((sys.argv[n]))
+print(sum)
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import hidden_4
+for element in dir(hidden_4):
+    print(element)
+if "__name__" == "__main__":
+    main()
