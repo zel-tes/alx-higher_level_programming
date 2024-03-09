d7d56b5d8623b9a9d84aa49ec9900390d8b2fc5e
zel-tes <zeltes18@gmail.com>
2024-03-09 00:54:41 -0800

    0x04_10 commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     total_score = 0
     total_weight = 0
+    if my_list == []:
+        return 0
     for score in my_list:
         total_score += score[0] * score[1]
         total_weight += score[1]
