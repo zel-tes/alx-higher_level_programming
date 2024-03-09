8499b30bd4a601e78579bc1dfd6eab3c273f8b44
zel-tes <zeltes18@gmail.com>
2024-03-08 22:10:35 -0800

    0x04_6 commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,25 +2,22 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
-    elif roman_string is not str:
-        return 0
     else:
         sum = 0
-        roman_num = []
-        roman_num.append(roman_string.split())
-        for i in range(len(roman_num)):
-            if roman_num[i] == 'I':
+        roman_num = list(roman_string)
+        for i in roman_num:
+            if i == 'I':
                 sum += 1
-            if roman_num[i] == 'V':
+            if i == 'V':
                 sum += 5
-            if roman_num[i] == 'X':
+            if i == 'X':
                 sum += 10
-            if roman_num[i] == 'L':
+            if i == 'L':
                 sum += 50
-            if roman_num[i] == 'C':
+            if i == 'C':
                 sum += 100
-            if roman_num[i] == 'D':
+            if i == 'D':
                 sum += 500
-            if roman_num[i] == 'M':
+            if i == 'M':
                 sum += 1000
-                return sum
+            return sum
