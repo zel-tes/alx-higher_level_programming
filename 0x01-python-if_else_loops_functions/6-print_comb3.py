ac518909ed26c413871e83509846f2989c1c5949
zel-tes <zeltes18@gmail.com>
2024-03-11 03:56:00 -0700

    0x01_1 commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i+1, 10):
+        print("{0}{1}, ".format(i, j), end="")
+        if (i == 8 and j == 9):
+            print("{0}{1}".format(i, j), end="\n")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if ord(x) in range(ord('A'), ord('Z')+1):
-            print(x, end="")
-        elif ord(x) in range(ord('a'), ord('z')+1):
-            x = char(x + 32)
-            print(x, end="")
+        if ord(x) not in range(97, 123):
+            print("{}".format(x), end="")
+        elif ord(x) in range(97, 123):
+            x = ord(x) - 32
+            print("{}".format(chr(x)), end="")
+    print()
