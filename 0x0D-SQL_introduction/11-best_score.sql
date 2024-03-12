c21316496cadc981af51019f352fd277022f4e9f
zel-tes <zeltes18@gmail.com>
2024-03-12 06:45:27 -0700

    0X0D_9 commit

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- list specific column
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updating score of BOB
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
