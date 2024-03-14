2f15e194ee93e742d809d54f07bf6ef0c5db4d40
zel-tes <zeltes18@gmail.com>
2024-03-14 06:55:10 -0700

    0x0E_13 commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,6 @@
--- lists shows with no genre
+-- lists shows with no
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id WHERE (
-	SELECT tv_genres.id
-	FROM tv_genres
- 	WHERE tv_genres.name = NULL
-)
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,9 @@
 -- count no of shows
-SELECT tv_genres.name AS "genre", SUM(tv_show_genres.show_id) AS "number_of_shows"
-FROM tv_genres INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name AS "genre", COUNT(tv_show_genres.show_id) AS "number_of_shows"
+FROM tv_genres LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id WHERE (
+	SELECT tv_shows.id 
+	FROM tv_shows
+	WHERE tv_show_genres.show_id = tv_shows.id
+)
 GROUP BY tv_genres.name
