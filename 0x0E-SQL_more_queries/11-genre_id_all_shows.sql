0098f0e051fd173d8fbc5910d5d3f6b795457bc1
zel-tes <zeltes18@gmail.com>
2024-03-14 02:36:27 -0700

    0x0E_10 commit

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- lists all show geners
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-LEFT JOIN tv_show_genres USING (genre_id)
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.ID = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
