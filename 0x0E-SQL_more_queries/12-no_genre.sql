b0aa9788d78c8917ad22c9253c1f71a8be9543b4
zel-tes <zeltes18@gmail.com>
2024-03-14 03:49:18 -0700

    0x0E_12 commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,9 @@
 -- lists shows with no genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres
-ON tv_show_genres.genre_id = NULL
+ON tv_shows.id = tv_show_genres.genre_id WHERE (
+	SELECT tv_genres.id
+	FROM tv_genres
+ 	WHERE tv_genres.name = NULL
+)
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- count no of shows
+SELECT tv_genres.name AS "genre", SUM(tv_show_genres.show_id) AS "number_of_shows"
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
