8c7591dd38f72bfcf62146e0cc74be5bc5843f05
zel-tes <zeltes18@gmail.com>
2024-04-08 06:59:02 -0700

    3rd commit JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const myVar = parseInt(process.argv[2])
+const myVar = parseInt(process.argv[2]);
 if (!process.argv[2]) {
   console.log('Not a number');
 } else if (!myVar) {
-	console.log('Not a number');
-}
-else {
-	console.log('My number: ' + myVar)
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myVar);
 }
