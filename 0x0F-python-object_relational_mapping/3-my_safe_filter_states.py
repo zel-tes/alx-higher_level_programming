889904cc2320d2ade14ee4c1cccf2899268da728
zel-tes <zeltes18@gmail.com>
2024-04-13 03:48:22 -0700

    9th commit python ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' the main() shows the states in the database "hbtn_0e_0_usa"
+in order of their id '''
+import MySQLdb
+import sys
+
+
+def main():
+    if (len(sys.argv) == 5):
+        username = sys.argv[1]
+        password = sys.argv[2]
+        name = sys.argv[3]
+        if (sys.argv[4] != ""):
+            state_1 = sys.argv[4]
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306, user=username,
+                passwd=password,
+                db=name)
+        cursor = db.cursor()
+        query = (''' SELECT states.id, states.name
+        FROM states
+        WHERE states.name = '{}'
+        ORDER BY states.id ASC
+        '''.format(state_1,))
+        cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
