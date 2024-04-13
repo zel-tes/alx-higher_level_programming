4fa4a05fab2c96b64225667b8cc81b08dd7ff8f7
zel-tes <zeltes18@gmail.com>
2024-04-13 02:10:27 -0700

    6th commit python ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' the main() shows the states in the database "hbtn_0e_0_usa"
+in order of their id '''
+import MySQLdb
+import sys
+
+
+def main():
+    if (len(sys.argv) == 4):
+        username = sys.argv[1]
+        password = sys.argv[2]
+        name = sys.argv[3]
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306, user=username,
+                passwd=password,
+                db=name)
+        cursor = db.cursor()
+        cursor.execute(''' SELECT states.id, states.name
+        FROM states
+        WHERE states.name LIKE 'N%'
+        ORDER BY states.id ASC
+        ''')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
