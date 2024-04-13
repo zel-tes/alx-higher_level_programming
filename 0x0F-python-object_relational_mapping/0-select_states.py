5735a58249a1858b5fbe7602b592a2b2f12320bb
zel-tes <zeltes18@gmail.com>
2024-04-13 01:25:48 -0700

    4th commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+''' the main() shows the states in the database "hbtn_0e_0_usa"
+in order of their id '''
 import MySQLdb
+import sys
 def main():
-    db = MySQLdb.connect(host="localhost", user="root", port=3306, password="root/root", database="hbtn_0e_0_usa")
-    cursor = db.cursor()
-    cursor.execute("SELECT states.id, states.name FROM states ORDER BY states.id ASC")
+    if (len(sys.argv) == 4):
+        username = sys.argv[1]
+        password = sys.argv[2]
+        name = sys.argv[3]
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=name)
+        cursor = db.cursor()
+        cursor.execute("SELECT states.id, states.name FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
