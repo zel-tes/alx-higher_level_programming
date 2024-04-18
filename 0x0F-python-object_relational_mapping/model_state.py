5ed6ca0a0585491464382606321f1c8211122164
zel-tes <zeltes18@gmail.com>
2024-04-18 04:08:31 -0700

    15th commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-engine = create_engine('sqlite:///hbtn_0e_6_usa')
+engine = create_engine('mysql://localhost:3306/hbtn_0e_6_usa')
 Base.metadata.create_all(engine)
